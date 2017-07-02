c4cd903c53c6d2a11f31db883339245929b99cc8
Nam Hoang Van (particle4dev) <particle4dev@gmail.com>
2017-07-02 16:43:33 +0700

    delete contact page

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -17,7 +17,6 @@ class Navigation extends React.Component {
   render() {
     return (
       <div className={s.root} role="navigation">
-        <Link className={s.link} to="/contact">Contact</Link>
         <span className={s.spacer}> | </span>
         <Link className={s.link} to="/login">Log in</Link>
         <span className={s.spacer}>or</span>
