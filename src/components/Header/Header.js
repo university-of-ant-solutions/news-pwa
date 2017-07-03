77b2def7d9cb967c8bd73a7110921daf47a80742
Nam Hoang Van (particle4dev) <particle4dev@gmail.com>
2017-07-03 10:32:06 +0700

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,3 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Header.css';
@@ -18,19 +9,18 @@ import logoUrl2x from './logo-small@2x.png';
 class Header extends React.Component {
   render() {
     return (
-      <div className={s.root}>
-        <div className={s.container}>
-          <Navigation />
-          <Link className={s.brand} to="/">
-            <img src={logoUrl} srcSet={`${logoUrl2x} 2x`} width="38" height="38" alt="React" />
-            <span className={s.brandTxt}>Your Company</span>
-          </Link>
-          <div className={s.banner}>
-            <h1 className={s.bannerTitle}>React</h1>
-            <p className={s.bannerDesc}>Complex web apps made easy</p>
-          </div>
-        </div>
-      </div>
+      <header className={s.header}>
+        <nav className={s.inner}>
+          <a href="/">
+            <img className={s.logo} src={logoUrl} srcSet={`${logoUrl2x} 2x`} width="24" height="24" alt="React" />
+          </a>
+          <a href="/top" className="router-link-exact-active router-link-active">Top</a>
+          <a href="/new">New</a>
+          <a href="/" target="_blank" rel="noopener" className={s.github}>
+            Built with ReactJS
+          </a>
+        </nav>
+      </header>
     );
   }
 }
