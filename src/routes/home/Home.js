13ce9b666c36dcecfb7ce8255806c38d54d181b9
Nam Hoang Van (particle4dev) <particle4dev@gmail.com>
2017-07-04 10:37:52 +0700

    not open new tab when click link

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -21,7 +21,7 @@ class Home extends React.Component {
             <li key={item.link} className={s.newsItem}>
               <span className={s.score}>35</span>
               <span className="title">
-                <Link to={`news/${item._id}`} target="_blank" rel="noopener">
+                <Link to={`news/${item._id}`} rel="noopener">
                   {item.title}
                 </Link>
                 <span className={s.host}> (zeptobars.com)</span>
