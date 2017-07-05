c491a869ec4eb82f2a26df6302487e4e9c4315e2
Nam Hoang Van (particle4dev) <particle4dev@gmail.com>
2017-07-05 22:51:34 +0700

    fix bug build Parsing error: 'import' and 'export' may only appear at the top level

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,14 +1,10 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-
 import { fromJS } from 'immutable';
 import createSagaMiddleware from 'redux-saga';
-
-
 import thunk from 'redux-thunk';
 import createReducer from '../reducers';
 import createHelpers from './createHelpers';
 import createLogger from './logger';
-
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -52,22 +48,22 @@ export default function configureStore(initialState = {}, helpersConfig) {
   // See https://github.com/rackt/redux/releases/tag/v3.1.0
   store = createStore(createReducer(), fromJS(initialState), enhancer);
 
-
   store.runSaga = sagaMiddleware.run;
   store.asyncReducers = {}; // Async reducer registry
-
 
   // Hot reload reducers (requires Webpack or Browserify HMR to be enabled)
   if (__DEV__ && module.hot) {
     module.hot.accept('../reducers', () => {
-      // eslint-disable-next-line global-require
       // store.replaceReducer(require('../reducers').default),
-      import('../reducers').then((reducerModule) => {
-        const createReducers = reducerModule.default;
-        const nextReducers = createReducers(store.asyncReducers);
+      /* eslint-disable global-require */
+      // eslint-disable-next-line global-require
+      // import('../reducers').then((reducerModule) => {
+        // const createReducers = reducerModule.default;
+      const createReducers = require('../reducers').default;
+      const nextReducers = createReducers(store.asyncReducers);
 
-        store.replaceReducer(nextReducers);
-      });
+      store.replaceReducer(nextReducers);
+      // });
     });
   }
 
