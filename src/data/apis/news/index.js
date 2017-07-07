531ea1b83c719612f07c0a6da17e59f1f92a3dc0
Nam Hoang Van (particle4dev) <particle4dev@gmail.com>
2017-07-07 15:58:33 +0700

    add more data

--- src/data/apis/news/index.js
+++ src/data/apis/news/index.js
@@ -6,6 +6,45 @@ import {
   PAGING_MAX,
 } from '../../../constants';
 
+import data from './data.json';
+
+(async () => {
+  try {
+    const { data: { news } } = data;
+    for (var i = news.length - 1; i >= 0; i--) {
+      let t = await NewsModel.findOne({ guid: news[i].guid });
+      if (!t) {
+        await NewsModel.create(news[i]);
+        console.log(`done: ${news[i].guid}`);
+      }
+    }
+  } catch (e) {
+    console.log(e);
+  }
+//   const buildingId = '58da279f0ff5af8c8be59c36';
+//   let user = '58f9c1bf2d4581000484b188';
+//   let t = await BuildingMembersModel.findOne({ building: buildingId, user });
+//   if (!t) {
+//     await BuildingMembersModel.create({
+//       building: buildingId,
+//       user,
+//       type: ADMIN,
+//       status: ACCEPTED,
+//     });
+//   }
+//   user = '59034c6c60f3c7beab57220a';
+//   await BuildingMembersModel.remove({ building: buildingId, user });
+//   t = await BuildingMembersModel.findOne({ building: buildingId, user });
+//   if (!t) {
+//     await BuildingMembersModel.create({
+//       building: buildingId,
+//       user,
+//       type: MEMBER,
+//       status: PENDING,
+//     });
+//   }
+})();
+
 const wrap = fn => (...args) => fn(...args).catch(args[2]);
 
 const newsService = Service({
