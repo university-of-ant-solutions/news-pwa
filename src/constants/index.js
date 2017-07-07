9aba2526e0fb6dbf5cb7bb21f7861e6b910ce535
Nam Hoang Van (particle4dev) <particle4dev@gmail.com>
2017-07-07 21:44:02 +0700

    set default page = 10

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 
 export const SET_RUNTIME_VARIABLE = 'SET_RUNTIME_VARIABLE';
-export const PAGING_DEFAULT = 7;
-export const PAGING_MAX = 14;
+export const PAGING_DEFAULT = 10;
+export const PAGING_MAX = 20;
 export const INITIALNOW = 'initialNow';
