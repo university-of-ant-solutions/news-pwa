579d282a21de63d9bbe82a202bdeb0491cc44643
Nam Hoang Van (particle4dev) <particle4dev@gmail.com>
2017-07-07 21:48:02 +0700

    update UI

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,12 +1,3 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Feedback.css';
@@ -18,12 +9,12 @@ class Feedback extends React.Component {
         <div className={s.container}>
           <a
             className={s.link}
-            href="https://gitter.im/kriasoft/react-starter-kit"
+            href="#"
           >Ask a question</a>
           <span className={s.spacer}>|</span>
           <a
             className={s.link}
-            href="https://github.com/kriasoft/react-starter-kit/issues/new"
+            href="#"
           >Report an issue</a>
         </div>
       </div>
