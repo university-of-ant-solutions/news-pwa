0630703785d7e301175da3011de05a0f451e4899
Nam Hoang Van (particle4dev) <particle4dev@gmail.com>
2017-07-07 10:36:18 +0700

    fixed build command remove Critical Rendering Path Performance Reduce render-blocking stylesheets 630 ms https://jakearchibald.com/2016/link-in-body/ https://github.com/filamentgroup/loadCSS

--- src/components/Html.js
+++ src/components/Html.js
@@ -53,12 +53,11 @@ class Html extends React.Component {
           <noscript><link rel="stylesheet" href="/offline-theme-chrome.css" /></noscript>
           <link rel="preload" href="/offline-language-english.css" as="style" onload="this.rel='stylesheet'"/>
           <noscript><link rel="stylesheet" href="/offline-language-english.css" /></noscript>
-          <script>
+          <script dangerouslySetInnerHTML={{ __html: `
           /*! loadCSS. [c]2017 Filament Group, Inc. MIT License */
           !function(e){"use strict";var n=function(n,t,o){function i(e){return a.body?e():void setTimeout(function(){i(e)})}function r(){l.addEventListener&&l.removeEventListener("load",r),l.media=o||"all"}var d,a=e.document,l=a.createElement("link");if(t)d=t;else{var s=(a.body||a.getElementsByTagName("head")[0]).childNodes;d=s[s.length-1]}var f=a.styleSheets;l.rel="stylesheet",l.href=n,l.media="only x",i(function(){d.parentNode.insertBefore(l,t?d:d.nextSibling)});var u=function(e){for(var n=l.href,t=f.length;t--;)if(f[t].href===n)return e();setTimeout(function(){u(e)})};return l.addEventListener&&l.addEventListener("load",r),l.onloadcssdefined=u,u(r),l};"undefined"!=typeof exports?exports.loadCSS=n:e.loadCSS=n}("undefined"!=typeof global?global:this);
           /*! loadCSS rel=preload polyfill. [c]2017 Filament Group, Inc. MIT License */
-          !function(t){if(t.loadCSS){var e=loadCSS.relpreload={};if(e.support=function(){try{return t.document.createElement("link").relList.supports("preload")}catch(e){return!1}},e.poly=function(){for(var e=t.document.getElementsByTagName("link"),r=0;r<e.length;r++){var n=e[r];"preload"===n.rel&&"style"===n.getAttribute("as")&&(t.loadCSS(n.href,n,n.getAttribute("media")),n.rel=null)}},!e.support()){e.poly();var r=t.setInterval(e.poly,300);t.addEventListener&&t.addEventListener("load",function(){e.poly(),t.clearInterval(r)}),t.attachEvent&&t.attachEvent("onload",function(){t.clearInterval(r)})}}}(this);
-          </script>
+          !function(t){if(t.loadCSS){var e=loadCSS.relpreload={};if(e.support=function(){try{return t.document.createElement("link").relList.supports("preload")}catch(e){return!1}},e.poly=function(){for(var e=t.document.getElementsByTagName("link"),r=0;r<e.length;r++){var n=e[r];"preload"===n.rel&&"style"===n.getAttribute("as")&&(t.loadCSS(n.href,n,n.getAttribute("media")),n.rel=null)}},!e.support()){e.poly();var r=t.setInterval(e.poly,300);t.addEventListener&&t.addEventListener("load",function(){e.poly(),t.clearInterval(r)}),t.attachEvent&&t.attachEvent("onload",function(){t.clearInterval(r)})}}}(this);` }} />
           {scripts.map(script => <link key={script} rel="preload" href={script} as="script" />)}
           <link rel="apple-touch-icon" href="apple-touch-icon.png" />
           {styles.map(style => (
